bf36f8fdca1488e921e3487fd0a63e41d330175f
BarbaraVera <113056190+BarbaraVera@users.noreply.github.com>
2024-03-13 16:04:52 -0300

    creacion de usuario y contraseña

--- App/crear_cliente/views.py
+++ App/crear_cliente/views.py
@@ -1,15 +1,58 @@
 from datetime import datetime
-from django.http import HttpRequest
-from django.shortcuts import render, redirect
-# Create your views here.
 
+import bcrypt
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render, redirect
+
+from App.inicial.models import empresa, usuario
+
+@login_required
 def crear_cliente(request):
     assert isinstance(request,HttpRequest)
+    empresas = empresa.objects.all()
     return render(
         request,
         'crear_cliente/crear_cliente.html',
         {
             'title':'Crear Cliente',
             'year':datetime.now().year,
+            'empresas':empresas,
         }
     )
+
+
+def insertar_cliente(request):
+    if request.method == 'POST':
+        nombre_cliente = request.POST.get('nombre_cliente')
+        usuario_cliente = request.POST.get('usuario_cliente')
+        contraseña_cliente = request.POST.get('contraseña_cliente')
+        empresa_cliente = request.POST.get('empresa_cliente')
+
+        datos_empresa = empresa.objects.get(cod_empresa=empresa_cliente)
+
+        existe_usuario = usuario.objects.filter(usuario=usuario_cliente).exists()
+
+        if existe_usuario:
+            messages.error(request, f'El nombre de usuario ya existe')
+            return redirect('inicio_crear_cliente')
+
+        hashed_password = bcrypt.hashpw(contraseña_cliente.encode('utf-8'), bcrypt.gensalt())
+
+        datos_cliente = usuario(
+            usuario=usuario_cliente,
+            clave=hashed_password.decode('utf-8'),
+            empresa=datos_empresa,
+            nombre=nombre_cliente
+        )
+        datos_cliente.save()
+        messages.success(request, 'El usuario se ha creado correctamente.')
+        return redirect('inicio_crear_cliente')
+
+    return redirect('inicio_crear_cliente')
+
+def existe_usuario(request, nombre_usuario):
+    usuario_existente = usuario.objects.filter(usuario=nombre_usuario)
+    data = list(usuario_existente.values())
+    return JsonResponse(data, safe=False)
